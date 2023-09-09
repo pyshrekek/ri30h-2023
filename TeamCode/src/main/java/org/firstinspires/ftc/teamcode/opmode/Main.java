aeb70786abe744b401268c89a4968f4d01c38b83
LOLFUTUREVEVO <Karolkopciuch05@gmail.com>
2023-09-09 14:57:39 -0400

    Cancer 😂😂😂😂😂😂😂
    fyuck

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Main.java
@@ -3,15 +3,19 @@ package org.firstinspires.ftc.teamcode.opmode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp
+@TeleOp(name = "TestIntakeColorSensor")
 public class Main extends LinearOpMode {
     ColorSensor testSensor;
     @Override
     public void runOpMode() throws InterruptedException {
+        // BLYAAAATTTTTZZZ NAHOYYY OK SUKA COLOR SENSOR BLYAT
          testSensor = hardwareMap.get(ColorSensor.class, "GetCol");
+        ElapsedTime time = new ElapsedTime();
          waitForStart();
          while(!isStopRequested()) {
+             telemetry.addData("Time", time.time());
              telemetry.addData("R",testSensor.red());
              telemetry.addData("G",testSensor.green());
              telemetry.addData("B",testSensor.blue());
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -8,32 +10,56 @@ public final class Intake {
     // 2 color sensors - One for color of gamepiece, One to detect if one is in
     // One Motor intake
     int currPassed = 0;
+    enum Colors{
+        GREEN,
+        PURPLE,
+        YELLOW,
+        WHITE
+    }
 
+    Colors[] storedPixels = {};
+    ColorSensor Point0,Point1;
+    DcMotor intake;
     final int maxVel = 100;
     double powerVal = 0;
+    Telemetry telemetry;
 
     public Intake(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        Point0= hardwareMap.get(ColorSensor.class, "GetCol");
+        Point1 = hardwareMap.get(ColorSensor.class, "IncrementPasses");
+    }
+    //This will literally only happen when there is more than 1 Pixel in the intake
+    public void checkPassing() {
+        if(Point1.red() < 15 &&Point1.green() < 15 && Point1.blue() < 15) {
+            telemetry.addData("NOTHING FOUND", "NULL");
+        } else {
+            storedPixels[currPassed-1] = processColor(Point1.red(),Point1.green(), Point1.blue());
+        }
+    }
 
+    Colors processColor(int r, int g, int b) {
+
+        return Colors.WHITE;
     }
 
 
+
+    // The following 2 methods either turn the intake on or off (Use it with conjunction to the amount of pixels in the bot
     public void Activate(double p) {
         powerVal = p;
     }
-
     public void Deactivate(double p) {
         powerVal = p;
     }
 
-
+    // increases the amount of pixels currently stored in the robot, the maximum of pixels can be changed so that you can more easily test things out
     public void incrementAmount() {
         currPassed++;
     }
+    // Clears the amount of pixels stored, so that you can intake more
     public void clearAmount() {
         currPassed = 0;
     }
-
-
-
-
 }
