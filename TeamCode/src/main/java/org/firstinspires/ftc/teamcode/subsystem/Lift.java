0edd65969b64a7de2305ddbf310cfd5314bed18a
pyshrekek <rukereero@gmail.com>
2023-09-09 14:47:15 -0400

    add lift back

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Lift.java
@@ -1,5 +1,66 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
-public class Lift {
+import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.BasicPID;
+import com.ThermalEquilibrium.homeostasis.Parameters.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+public class Lift extends Mechanism {
+    DcMotorEx[] motors = new DcMotorEx[2];
+    ElapsedTime timer = new ElapsedTime();
+
+    public static double kP = 0;
+    public static double kI = 0;
+    public static double kD = 0;
+
+    public static double bottom = 0;
+    public static double up = 300;
+
+    public static double target = 0;
+    public double power;
+
+    private static final double WHEEL_RADIUS = 1.37795;
+    private static final double GEAR_RATIO = 1.0;
+    private static final double TICKS_PER_REV = 145.1;
+
+    @Override
+    public void init(HardwareMap hwMap) {
+        motors[0] = hwMap.get(DcMotorEx.class, "left");
+        motors[1] = hwMap.get(DcMotorEx.class, "right");
+        motors[0].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motors[1].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motors[0].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motors[1].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motors[0].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motors[1].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motors[0].setDirection(DcMotorSimple.Direction.FORWARD);
+        motors[1].setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void setTargetPosition(double pos) {
+        target = pos;
+    }
+
+    public void loop() {
+        PIDCoefficients coefficients = new PIDCoefficients(kP, kI, kD);
+        BasicPID controller = new BasicPID(coefficients);
+        power = controller.calculate(target, motors[0].getCurrentPosition());
+        motors[0].setPower(power);
+        motors[1].setPower(power);
+    }
+
+    public double ticksToInches(double ticks) {
+        return 2 * Math.PI * WHEEL_RADIUS * GEAR_RATIO * ticks / TICKS_PER_REV;
+    }
+
+    public double getPos() {
+        return motors[0].getCurrentPosition();
+    }
+
+    public double getPosInches() {
+        return ticksToInches(getPos());
+    }
 }
