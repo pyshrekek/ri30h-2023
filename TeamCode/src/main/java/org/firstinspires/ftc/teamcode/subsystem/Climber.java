805707a40bb9414f651ee3d095ae4c207f08a945
warren yun <wyun2006@gmail.com>
2023-09-09 13:54:24 -0400

    basic climber rofl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Climber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Climber.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
-public class Climber {
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.hardware.lynx.LynxModule;
+
+public class Climber extends Mechanism {
+
+    private DcMotorEx climb;
+
+    @Override
+    public void init(HardwareMap hwMap) {
+        for (LynxModule module : hwMap.getAll(LynxModule.class)) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+        this.climb = hwMap.get(DcMotorEx.class, "climber");
+    }
+
+    public void moveClimber(Gamepad gamepad) {
+        climb.setPower(gamepad.right_stick_y);
+    }
+
 }
